c50d30870c80496c5c15764775cc0ba0870f97b9
Ahmed Abdullah <abdullahahmad6782@gmail.com>
2026-07-19 18:00:50 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -100,7 +100,7 @@ export default function Home({ setActiveTab }) {
                 <img
                   className="w-full h-full object-cover"
                   src="https://images.unsplash.com/photo-1497215641119-bbe6d71ebaae?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  alt="Ahmed Abdullah portrait"
+                  alt="image home screen"
                   referrerPolicy="no-referrer"
                 />
               </div>
